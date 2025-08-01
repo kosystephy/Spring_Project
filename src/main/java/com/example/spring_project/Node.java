f0109f8d50dbfa32db3d6f8f76f7d23267577d0d
kosystephy <kosysteve03@gmail.com>
2025-08-01 10:33:10 +0100

    Creating and populating the node class

---
+++ src/main/java/com/example/spring_project/Node.java
@@ -0,0 +1,65 @@
+package com.example.spring_project;
+import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+@Entity
+public class Node { @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String location;
+    private int latitude;
+    private int longitude;
+
+    public Node(String name, String location, int longitude, int latitude) {
+         this.name = name;
+        this.location = location;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public Node() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(int latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+}
--- src/main/java/com/example/spring_project/SpringProjectApplication.java
+++ src/main/java/com/example/spring_project/SpringProjectApplication.java
@@ -9,7 +9,7 @@ public class SpringProjectApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(SpringProjectApplication.class, args);
-        System.out.println();
+
     }
 
 }
