d66623f814cbfc85fba3ac0cafce8d2334bf4b22
Raveena Rathnam <118574384+RaveenaRathnam@users.noreply.github.com>
2025-08-01 11:55:41 +0100

    Undo the Test.

--- src/test/java/com/example/spring_project/SpringProjectApplicationTests.java
+++ src/test/java/com/example/spring_project/SpringProjectApplicationTests.java
@@ -1,22 +1,13 @@
 package com.example.spring_project;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class SpringProjectApplicationTests {
 
-    @Autowired
-    private MockMvc mockMvc;
+    @Test
+    void contextLoads() {
+    }
 
-//    @Test
-//    public void testAddNode() throws Exception {
-//        mockMvc.perform(post("/api/nodes")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"name\":\"Node1\",\"location\":\"Dublin\",\"latitude\":10,\"longitude\":20}"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.id").exists());
-//    }
 }
