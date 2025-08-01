8b1139724503380d48b401c6900c83e49bdeda27
kosystephy <kosysteve03@gmail.com>
2025-08-01 11:41:03 +0100

    Node Service class update

--- src/main/java/com/example/spring_project/NodeService.java
+++ src/main/java/com/example/spring_project/NodeService.java
@@ -3,4 +3,8 @@ package com.example.spring_project;
 import java.util.Map;
 
 public interface NodeService {
+    Node addNode(Node newNode);
+    boolean deleteNode(int id);
+    Node updateNode(int id,Node updatedNode);
+    Node getNodeById(int id);
 }
