6d2dd753169dbe950d368982a7342755a0e56612
kosystephy <kosysteve03@gmail.com>
2025-08-01 12:28:14 +0100

    Adding the CRUD Repo class and using the methods to perform operations on the controller

--- src/main/java/com/example/spring_project/NodeController.java
+++ src/main/java/com/example/spring_project/NodeController.java
@@ -1,32 +1,56 @@
 package com.example.spring_project;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/nodes")
 public class NodeController {
 
     @Autowired
-    private NodeServiceImpl nodeService;
+    private NodeCrudRepository crudRepository;
 
     @PostMapping
-    public Node addNode(@RequestBody Node node) {
-        return nodeService.addNode(node);
+    public ResponseEntity<Node> addNode(@RequestBody Node node) {
+        node= crudRepository.save(node);
+        URI uri = URI.create("/node/" + node.getId());
+        return ResponseEntity.created(uri).body(node);
     }
 
     @GetMapping("/{id}")
-    public Node getNodeById(@PathVariable int id) {
-        return nodeService.getNodeById(id);
+    public ResponseEntity<Node> getNodeById(@PathVariable int id) {
+
+        Optional<Node> node = crudRepository.findById(id);
+        if(node.isEmpty())
+        {
+            return ResponseEntity.notFound().build();
+        }
+        else{
+            return ResponseEntity.ok().body(node.get());
+        }
     }
 
     @PutMapping("/{id}")
-    public Node updateNode(@PathVariable int id, @RequestBody Node node) {
-        return nodeService.updateNode(id, node);
+    public ResponseEntity<Node> updateNode(@PathVariable int id, @RequestBody Node node) {
+        if( crudRepository.updateNode(id, node) == 0){
+            return ResponseEntity.notFound().build();
+        }
+        else{
+            return ResponseEntity.ok().body(node);
+        }
     }
 
     @DeleteMapping("/{id}")
-    public String deleteNode(@PathVariable int id) {
-        return nodeService.deleteNode(id) ? "Node deleted" : "Node not found";
+    public ResponseEntity<String> deleteNode(@PathVariable int id) {
+        Optional<Node> node = crudRepository.deleteNode(id);
+        if(node.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        else{
+            return ResponseEntity.ok().body(node.get().getName());
+        }
     }
 }
---
+++ src/main/java/com/example/spring_project/NodeCrudRepository.java
@@ -0,0 +1,7 @@
+package com.example.spring_project;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface NodeCrudRepository extends CrudRepository<Node,Integer> {
+
+}
